6ed4972ed05908df1f69dfa38fbb99a9d5347bde
Malte Fillmer <malte.fillmer@gmail.com>
2021-12-08 15:22:44 +0000

    add closing mechanism

--- src/SectionRating.tsx
+++ src/SectionRating.tsx
@@ -1,14 +1,16 @@
 import { Drawer, Typography } from '@mui/material'
 import React, { FC } from 'react'
 import { SectionItem } from './redux/sections'
-import { useCurrentItem } from './Section.util'
+import { useCurrentItem, useSetItem } from './Section.util'
 import { SectionCommentInput } from './SectionCommentInput'
 import { SectionRatingInput } from './SectionRatingInput'
 
 export const SectionRating: FC = () => {
   const item = useCurrentItem()
+  const setItem = useSetItem()
+  const onClick = () => setItem(undefined)
   return (
-    <Drawer anchor='bottom' open={!!item}>
+    <Drawer anchor='bottom' open={!!item} onClick={onClick}>
       <Typography variant='h3'>{item?.name}</Typography>
       <SectionRatingInput uuid={item?.uuid || ''} />
       <SectionCommentInput uuid={item?.uuid || ''} />
