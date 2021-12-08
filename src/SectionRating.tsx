b0f171775d76913b5abce7de323b28ff9bd327da
Malte Fillmer <malte.fillmer@gmail.com>
2021-12-08 20:26:37 +0000

    add icons

--- src/SectionRating.tsx
+++ src/SectionRating.tsx
@@ -1,18 +1,29 @@
-import { Drawer, Typography } from '@mui/material'
+import { Close } from '@mui/icons-material'
+import { Drawer, IconButton, Stack, Typography, Button } from '@mui/material'
+import { Box } from '@mui/system'
 import React, { FC } from 'react'
 import { useCurrentItem, useSetItem } from './Section.util'
 import { SectionCommentInput } from './SectionCommentInput'
 import { SectionRatingInput } from './SectionRatingInput'
 
 export const SectionRating: FC = () => {
-  const item = useCurrentItem()
-  const setItem = useSetItem()
-  const onClick = () => setItem(undefined)
-  return (
-    <Drawer anchor='bottom' open={!!item} onClick={onClick}>
-      <Typography variant='h3'>{item?.name}</Typography>
-      <SectionRatingInput uuid={item?.uuid || ''} />
-      <SectionCommentInput uuid={item?.uuid || ''} />
-    </Drawer>
-  )
+    const item = useCurrentItem()
+    const setItem = useSetItem()
+    const onClick = () => setItem(undefined)
+    return (
+        <Drawer
+            variant='persistent'
+            sx={{ borderTopLeftRadius: 3, borderTopRightRadius: 3 }}
+            anchor='bottom' open={!!item} >
+            <Box sx={{ padding: 2 }} >
+                <Stack direction='row'>
+                    <Typography variant='h3' title={item?.name} noWrap>{item?.name}</Typography>
+                    <IconButton onClick={onClick}><Close /></IconButton>
+                </Stack>
+                <SectionRatingInput uuid={item?.uuid || ''} />
+                <SectionCommentInput uuid={item?.uuid || ''} />
+                <Button variant='outlined' fullWidth onClick={onClick}>Fertig</Button>
+            </Box>
+        </Drawer>
+    )
 }
--- src/SectionRatingInput.tsx
+++ src/SectionRatingInput.tsx
@@ -1,40 +1,28 @@
-import { Button, ButtonGroup, Chip } from '@mui/material'
-import { Box } from '@mui/system'
+import { ToggleButton, ToggleButtonGroup, Typography } from '@mui/material'
 import React, { FC } from 'react'
 import { useSectionRating } from './Rating.utils'
 
 interface props {
-  uuid: string
+    uuid: string
 }
 
 export const SectionRatingInput: FC<props> = ({ uuid }) => {
-  const items = Array(4).fill(0)
-  const { rating, setRating } = useSectionRating(uuid)
+    const items = Array(4).fill(0)
+    const { rating, setRating } = useSectionRating(uuid)
 
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        '>*': { marginRight: 2 },
-      }}
-      onClick={(e) => e.stopPropagation()}
-    >
-      {rating !== undefined && (
-        <Chip onClick={() => setRating(undefined)} label={rating} />
-      )}
-      <ButtonGroup variant='outlined'>
-        {rating === undefined &&
-          items.map((_, index) => (
-            <Button
-              variant='outlined'
-              onClick={() => setRating(index)}
-              key={index}
-            >
-              {index}
-            </Button>
-          ))}
-      </ButtonGroup>
-    </Box>
-  )
+    return (<div>
+        <Typography variant='subtitle1'>Stresslevel</Typography>
+        <ToggleButtonGroup fullWidth value={rating} size='large' >
+            {items.map((_, index) => (
+                <ToggleButton
+                    fullWidth
+                    value={index}
+                    onClick={() => setRating(index)}
+                    key={index}
+                >{index}</ToggleButton>
+            ))}
+        </ToggleButtonGroup>
+    </div>
+
+    )
 }
