27667c4f99a955ed7016618bfb2f3a622d8ec092
Malte Fillmer <malte.fillmer@gmail.com>
2021-12-08 20:32:40 +0000

    renaming

---
+++ src/AddSection.tsx
@@ -0,0 +1,36 @@
+import { Button, TextField, Box } from '@mui/material'
+import React, { useState } from 'react'
+import { useAdd } from './Section.util'
+
+export const AddSection = () => {
+    const [value, setValue] = useState('')
+    const add = useAdd(value)
+    const reset = () => setValue('')
+    const onSubmit = () => {
+        add()
+        reset()
+    }
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'flex-start',
+                marginY: 2,
+                width: '100%',
+            }}
+        >
+            <TextField
+                label='neuer Bereich'
+                variant='outlined'
+                type='text'
+                sx={{ marginRight: 2 }}
+                size='small'
+                value={value}
+                onChange={(e) => setValue(e.currentTarget.value)}
+            />
+            <Button variant='outlined' onClick={onSubmit}>
+                hinzufügen
+            </Button>
+        </Box>
+    )
+}
