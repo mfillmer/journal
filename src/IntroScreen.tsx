be4dde5d85f719440c5117c1e6996735ad629095
Malte Fillmer <malte.fillmer@gmail.com>
2021-12-13 11:25:53 +0000

    add Intro text

--- src/IntroScreen.tsx
+++ src/IntroScreen.tsx
@@ -7,13 +7,31 @@ export const IntroScreen = () => {
   return (
     <Box display='flex' flexDirection='column' alignItems='center'>
       <Typography variant='h1' textAlign='center' marginY={4}>
-        Journal
+        Das ist dein Journal
       </Typography>
       <Typography variant='body1' textAlign='justify' marginBottom={10}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et ab a
-        accusamus quod placeat id exercitationem. Distinctio accusamus
-        blanditiis animi assumenda neque mollitia alias corporis, iure illum
-        odio similique maxime.
+        <Typography paragraph>
+          <Typography paragraph>
+            Schreibe auf was dich beschäftigt, damit dein Kopf frei bleibt.
+          </Typography>
+          <Typography paragraph>
+            Ganz nach dem Prinzip <i>Teile und Herrsche</i> kannst du hier
+            verschiedene Bereiche deines Lebens eintragen und dich jedem einzeln
+            widmen.
+          </Typography>{' '}
+          <Typography paragraph>
+            Für jeden Bereich kannst du das entsprechende Stresslevel bewerten
+            und dir Notizen machen. Du kannst auch Bereiche weiter unterteilen,
+            für noch mehr Fokus.
+          </Typography>
+        </Typography>
+        <Typography paragraph>
+          Last but not least: Deine Daten sind sicher! Alles bleibt auf diesem
+          Gerät.
+        </Typography>
+      </Typography>
+      <Typography variant='subtitle1' marginBottom={2}>
+        Starte jetzt
       </Typography>
       <SectionQuickAdd />
     </Box>
