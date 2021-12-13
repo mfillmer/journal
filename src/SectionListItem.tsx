d3827d914f0e8c6274c02f2c256f8ec7a2cede51
Malte Fillmer <malte.fillmer@gmail.com>
2021-12-13 13:48:37 +0000

    show comments

--- src/SectionListItem.tsx
+++ src/SectionListItem.tsx
@@ -1,6 +1,7 @@
 import { ArrowForwardIos } from '@mui/icons-material'
 import { IconButton, Stack, Typography } from '@mui/material'
 import React, { FC } from 'react'
+import { useSectionComment } from './Rating.utils'
 import { SectionItem } from './redux/sections'
 import { useHasChildren, usePath, useSetItem, useSetPath } from './Section.util'
 import { useRatingColor } from './useRatingColor'
@@ -9,6 +10,7 @@ export const SectionListItem: FC<{ item: SectionItem }> = ({ item }) => {
   const path = usePath()
   const setPath = useSetPath()
   const setItem = useSetItem()
+  const { comment } = useSectionComment(item.uuid)
   const bgColor = useRatingColor(item.uuid)
   const hasChildren = useHasChildren(item)
   const onClick = () => {
@@ -23,9 +25,23 @@ export const SectionListItem: FC<{ item: SectionItem }> = ({ item }) => {
       alignItems='center'
       marginY={1}
       onClick={onClick}>
-      <Typography noWrap variant='h2' sx={{ flex: 1 }}>
+      <Typography
+        variant='h2'
+        noWrap={!comment}
+        marginRight={1}
+        sx={{
+          flex: 1,
+          wordBreak: 'break-all',
+          transition: 'all',
+          fontSize: !comment ? undefined : '1rem',
+        }}>
         {item.label}
       </Typography>
+      {comment && (
+        <Typography variant='body1' flex='5' sx={{ wordBreak: 'break-all' }}>
+          {comment}
+        </Typography>
+      )}
       <IconButton>
         <ArrowForwardIos />
       </IconButton>
