8db921775edf3a33b2c89741f602cd93abb98b9b
Malte Fillmer <malte.fillmer@gmail.com>
2021-12-19 09:46:12 +0000

    add labels for subsections

--- src/hooks/useChildComments.tsx
+++ src/hooks/useChildComments.tsx
@@ -1,6 +1,11 @@
+import { useAppSelector } from '../redux/store'
 import { useChildrenRatings } from './useChildrenRatings'
 
 export const useChildComments = (uuid: string) => {
+  const sections = useAppSelector((state) => state.sections)
+
   const childRatings = useChildrenRatings(uuid)
-  return childRatings.map((c) => c?.comment)
+  return childRatings
+    .map((c) => ({ ...c, label: sections[c.section]?.label || '' }))
+    .map((c) => `${c.label && c.label + ':'} ${c?.comment}`.trim())
 }
